26bd7436bcac73610d97d39e00199cb9fc68ca3e
Мария Шапошникова <m.a.shap58000@gmail.com>
2025-04-17 19:50:28 +0300

    start

---
+++ lms/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название курса")
+    preview = models.ImageField(upload_to="media/img/", blank=True, null=True, verbose_name="Превью")
+    description = models.TextField(verbose_name="Описание")
+
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
+
+    class Meta:
+        verbose_name = "курс"
+        verbose_name_plural = "курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название урока")
+    description = models.TextField()
+    preview = models.ImageField(upload_to="media/img/", blank=True, null=True, verbose_name="Превью")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
+
+    class Meta:
+        verbose_name = "урок"
+        verbose_name_plural = "уроки"
+
