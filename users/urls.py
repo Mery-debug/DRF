3f1eeaed799a0a4dc8efe6fec7d62f89607d5cd7
Mery-debug <m.a.shap58000@gmail.com>
2025-05-05 18:53:53 +0300

    1

--- users/urls.py
+++ users/urls.py
@@ -11,6 +11,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("payment/", PaymentListAPIView.as_view(), name="payment_list"),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
