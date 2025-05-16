999b6b7cf6e96ad59006631b5d8a1b4b9ad566e7
Mery-debug <m.a.shap58000@gmail.com>
2025-05-17 01:11:24 +0300

    4

--- lms/models.py
+++ lms/models.py
@@ -6,7 +6,6 @@ class Course(models.Model):
     name = models.CharField(max_length=150, verbose_name="Название курса")
     preview = models.ImageField(upload_to="media/img/", blank=True, null=True, verbose_name="Превью")
     description = models.TextField(verbose_name="Описание")
-    video_url = models.CharField(null=True, blank=True, verbose_name="ссылка на видео-материал")
 
     def __str__(self):
         return f"{self.name} {self.description}"
@@ -21,6 +20,7 @@ class Lesson(models.Model):
     description = models.TextField()
     preview = models.ImageField(upload_to="media/img/", blank=True, null=True, verbose_name="Превью")
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+    video_url = models.CharField(null=True, blank=True, verbose_name="ссылка на видео-материал")
 
     def __str__(self):
         return f"{self.name} {self.description}"
