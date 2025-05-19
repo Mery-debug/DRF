0b2afbadc184256acae8198973321e1f8f64e65e
Mery-debug <m.a.shap58000@gmail.com>
2025-05-19 15:57:00 +0300

    2

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'EXCEPTION_HANDLER': 'users.api_errors.ServiceUnavailable',
 }
 
 SIMPLE_JWT = {
--- users/api_errors.py
+++ users/api_errors.py
@@ -1,7 +1,27 @@
+from django.db import DatabaseError
+from rest_framework.response import Response
 from rest_framework.exceptions import APIException
+from rest_framework.views import exception_handler
 
 
 class ServiceUnavailable(APIException):
     status_code = 503
     default_detail = 'Service temporarily unavailable, try again later.'
     default_code = 'service_unavailable'
+
+
+def custom_exception_handler(exc, context):
+    """Кастомный обработчик исключений"""
+    response = exception_handler(exc, context)
+
+    if isinstance(exc, (DatabaseError, TimeoutError)):
+        return Response(
+            {
+                "error": "Service Unavailable",
+                "detail": str(exc),
+                "code": "service_unavailable"
+            },
+            status=503
+        )
+
+    return response
